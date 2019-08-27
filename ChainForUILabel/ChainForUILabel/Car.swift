2708b73388d01be7278c363387e5c8291438aa98
Howeix <495658492@qq.com>
2019-08-27 17:57:03 +0800

    add person & car

---
+++ ChainForUILabel/ChainForUILabel/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  ChainForUILabel
+//
+//  Created by Howeix on 2019/8/27.
+//  Copyright © 2019 Howeix. All rights reserved.
+//
+
+import UIKit
+
+class Car: NSObject {
+
+}
---
+++ ChainForUILabel/ChainForUILabel/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  ChainForUILabel
+//
+//  Created by Howeix on 2019/8/27.
+//  Copyright © 2019 Howeix. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
